74719bc1a6642b9a359bdbb6328b29d483b0d958
aolteanu <aolteanu@tremend.ro>
2014-09-23 11:17:11 +0300

    - execute shell script to run photoscan

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -48,7 +48,7 @@ class SiteController extends Controller
         if(file_exists(Yii::app()->getBasePath() . '/../uploads/process.sh')) {
             chdir(Yii::app()->getBasePath() . '/../uploads/');
             echo shell_exec('pwd');
-            var_dump(shell_exec('export DISPLAY=:1 && ./process.sh 2>&1'));
+            echo shell_exec('export DISPLAY=:1 && ./process.sh 2>&1');
         }
     }
 
